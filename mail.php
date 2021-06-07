c0f06bf3c222a49b43c73641a082916dc33a5d5b
Kamyshnikov Denis <82932655+kavolk@users.noreply.github.com>
2021-06-07 09:24:58 +0300

    в312

--- mail.php
+++ mail.php
@@ -4,26 +4,15 @@
 		$name = $_POST['name'];
 	else die ("Не заполнено поле \"Имя\"");//если же атрибут пустой или не существует то завершаем выполнение скрипта и выдаем ошибку пользователю.
 
-	if (isset($_POST['email']) &amp;&amp; $_POST['email'] != "") //тут все точно так же как и в предыдущем случае
-		$email = $_POST['email'];
-	else die ("Не заполнено поле \"Email\"");
-
-	if (isset($_POST['subjects']) &amp;&amp; $_POST['subjects'] != "") 
-		$sub = $_POST['subjects'];
-	else die ("Не заполнено поле \"Тема\"");
-
-	if (isset($_POST['message']) &amp;&amp; $_POST['message'] != "") 
-		$body = $_POST['message'];
-	else die ("Не заполнено поле \"Сообщение\"");
-	 
+	if (isset($_POST['phone']) &amp;&amp; $_POST['phone'] != "") //тут все точно так же как и в предыдущем случае
+		$phone = $_POST['phone'];
+	else die ("Не заполнено поле \"phone\"");
 
 
 	$address = "kam.dena@yandex.ru";//адрес куда будет отсылаться сообщение для администратора
 	$mes  = "Имя: $name \n";	//в этих строчках мы заполняем текст сообщения. С помощью оператора .= мы просто дополняем текст в переменную
-	$mes .= "E-mail: $email \n";
- 	$mes .= "Тема: $sub \n";
- 	$mes .= "Текст: $body"; 
-	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");//собственно сам вызов функции отправки сообщения на сервере
+	$mes  = "Телефон: $phone \n";
+	$send = mail ($address,$mes);//собственно сам вызов функции отправки сообщения на сервере
 
 	if ($send) //проверяем, отправилось ли сообщение
 		echo "Сообщение отправлено успешно! Перейти на <a href='https://You-hands.ru/'>you-hands.ru</a>, если вас не перенаправило вручную.";
